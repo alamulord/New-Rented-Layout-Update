39fd6295d070beb709c00ea7de1f6f94ab9b2668
alamulord <alamulord@gmail.com>
2024-01-19 17:09:49 +0100

    reset pwd

--- app/account_auth/pass-forget/page.jsx
+++ app/account_auth/pass-forget/page.jsx
@@ -1,29 +1,34 @@
 export default function page() {
-  return (
-    <div className="container">
-        <div className="signup_form">
-            <div className="mt-5 bg-fuchsia-400 rounded-lg shadow-lg p-4 form_details" >
-                <h1 className="text-center text-white font-medium">RESET PASSWORD</h1>
-                <form className="space-y-6" action="#" method="POST">
-                    <div className="form_split">
-                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address:</label>
-                        <div className="mt-2">
-                        <input id="email" name="email" type="email" required placeholder="example@gmail.com" 
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
-                             ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-                    <div>
-                        <button type="submit" 
-                            className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 
-                            text-sm font-semibold leading-6 text-fuchsia-700 shadow-sm hover:bg-indigo-500 focus-visible:outline 
-                            focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 hover:text-white ">
-                                Reset Password
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-  )
+	return (
+		<div className="bg-fuchsia-300 py-5 mb-0">
+			<form class="w-full max-w-lg mx-auto mt-10 bg-white rounded p-5">
+				<p class="text-slate-600 text-2xl font-[800] leading-10 mb-10 text-center">
+					Forget Password
+				</p>
+
+				<div class="flex flex-wrap -mx-3 mb-6">
+					<div class="w-full px-3">
+						<label
+							class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+							for="grid-password">
+							Forget Password
+						</label>
+						<input
+							class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+							id="grid-password"
+							type="email"
+							placeholder="Your Email Address"
+							required
+						/>
+					</div>
+				</div>
+
+				<button
+					type="submit"
+					className="text-black p-2  bg-white w-full mt-3 font-[700] border-2 border-fuchsia-200 rounded-md basline hover:bg-fuchsia-500 hover:text-whites">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
